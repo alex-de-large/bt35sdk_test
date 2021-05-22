c0308f63b28e3c91408044fd4cf024333dcc7db3
alex-de-large <dmitriy.antakov@mail.ru>
2021-05-22 23:43:44 +0300

    m

--- app/src/main/java/com/example/bt35_sdk_test/QrCodeReader.java
+++ app/src/main/java/com/example/bt35_sdk_test/QrCodeReader.java
@@ -1,10 +1,14 @@
 package com.example.bt35_sdk_test;
 
 import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.common.InputImage;
 
 public class QrCodeReader {
 
+    private BarcodeScanner barcodeScanner;
     private BarcodeScannerOptions options;
 
     public interface OnScanSuccess {
@@ -21,10 +25,20 @@ public class QrCodeReader {
                         Barcode.FORMAT_QR_CODE,
                         Barcode.FORMAT_AZTEC)
                 .build();
+        barcodeScanner = BarcodeScanning.getClient(options);
+    }
+
+    public void scan(byte[] data, int width, int height, int rotation) {
 
     }
 
-    public void scan(byte[] data) {
-
+    private InputImage getImage(byte[] data, int width, int height, int rotation) {
+        return InputImage.fromByteArray(
+                data,
+                width,
+                height,
+                rotation,
+                InputImage.IMAGE_FORMAT_NV21
+        );
     }
 }
